8f0dd7e7708a063ea65b96ee34db798e29898830
Alex G <aleksey.gogolev@gmail.com>
2025-04-09 21:42:11 -0600

    Vostok temperature vertical chart next to the mail timeline.

--- src/index.js
+++ src/index.js
@@ -6,9 +6,13 @@ import axisBottom from "d3-axis";
 import * as d3 from "d3";
 import * as Data from "./data.js";
 
-var chart = new d3KitTimeline("#timeline", {
+const TIMELINE_HEIGHT = 3000;
+
+// Configure the main timeline.
+
+var chart = new d3KitTimeline("#main_timeline", {
   direction: "right",
-  initialHeight: 3000,
+  initialHeight: TIMELINE_HEIGHT,
   layerGap: 40,
   dotColor: (d) => d.color,
   labelBgColor: (d) => d.color,
@@ -27,14 +31,15 @@ var chart = new d3KitTimeline("#timeline", {
 // Prepare to handle tooltip.
 const Tooltip = document.getElementById("tooltip");
 const ChartDiv = document.getElementsByClassName("d3kit-chart-root")[0];
-const absoluteTop = ChartDiv.getBoundingClientRect().top + window.scrollY;
-const absoluteLeft = ChartDiv.getBoundingClientRect().left + window.scrollX;
 
 // Three function that change the tooltip when user hover / move / leave a cell
 var mouseover = function (event) {
   console.log(event);
-  const x = event.x + event.w + absoluteLeft;
-  const y = event.y - event.h + absoluteTop;
+  const absoluteTop = ChartDiv.getBoundingClientRect().top;
+  const absoluteLeft = ChartDiv.getBoundingClientRect().left;
+  const offset = 75;
+  const x = absoluteLeft + offset + event.w;
+  const y = absoluteTop + event.y - event.h;
 
   Tooltip.style.left = `${x + 10}px`; // Adjust position as needed
   Tooltip.style.top = `${y + 10}px`; // Adjust position as needed
@@ -74,64 +79,55 @@ chart
 // Render Vostok temperatures.
 
 function renderVostokTemps(data) {
-    // set the dimensions and margins of the graph
-var margin = { top: 10, right: 30, bottom: 30, left: 60 };
-var width = 460 - margin.left - margin.right;
-var height = 400 - margin.top - margin.bottom;
+  // set the dimensions and margins of the graph
+  var margin = { top: 0, right: 30, bottom: 0, left: 60 };
+  var width = 250 - margin.left - margin.right;
+  var height = TIMELINE_HEIGHT - margin.top - margin.bottom;
 
-// append the svg object to the body of the page
-var svg = d3
-  .select("#timeline2")
-  .append("svg")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-// const data = [{"date":2000, "temp": -8}, {"date": 80000, "temp":8}];
+  // append the svg object to the body of the page
+  var svg = d3
+    .select("#temperature_timeline")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  // const data = [{"date":new Date(-100000, 0, 1), "temp": -8}, {"date": new Date(0, 0, 1), "temp":4}];
 
-console.log(1111);
-console.log(data);
+  // Add X axis 
+  var xScale = d3
+    .scaleLinear()
+    .domain([-10, 4])
+    .range([0, width]);
+  svg.append("g").call(d3.axisBottom(xScale));
 
-// Add X axis 
-var xScale = d3
-  .scaleLinear()
-  .domain([2000, 100000])
-  .range([0, width]);
+  // Add Y axis
+  var yScale = d3
+    .scaleTime()
+    .domain([new Date(-100000, 0, 1), new Date(0, 0, 1)])
+    .range([height, 0]);
+  svg
+    .append("g")
+    .call(d3.axisLeft(yScale).tickFormat((x) => x.getFullYear()));
 
-svg
-  .append("g")
-  .attr("transform", "translate(0," + height + ")")
-  .call(d3.axisBottom(xScale));
+  const lineAccessor = d3.line().x(d => xScale(d.temp)).y(d => yScale(d.date));
 
-// Add Y axis
-var yScale = d3
-  .scaleLinear()
-  .domain([-10.1, 10.1])
-  .range([height, 0]);
+  // Add the line
+  svg
+    .append("path")
+    .data([data])
+    .attr("fill", "none")
+    .attr("stroke", "steelblue")
+    .attr("stroke-width", 1.5)
+    .attr("d", lineAccessor);
+};
 
-svg.append("g").call(d3.axisLeft(yScale));
-
-const lineAccessor = d3.line().x(d => xScale(d.date)).y(d => yScale(d.temp));
-
-console.log(2222);
-console.log(lineAccessor(data[0]));
-
-// Add the line
-svg
-  .append("path")
-  .data([data])
-  .attr("fill", "none")
-  .attr("stroke", "steelblue")
-  .attr("stroke-width", 1.5)
-  .attr("d", lineAccessor);
-
-  };
 
 const data = d3.csv(
   "static/vostok_10000.csv",
   // When reading the csv, format variables:
   function (d) {
-    return { date: parseInt(d.year), temp: parseFloat(d.temp) };
+    return { date: new Date(-d.year, 0, 1), temp: parseFloat(d.temp) };
   },
 ).then(
   renderVostokTemps);
